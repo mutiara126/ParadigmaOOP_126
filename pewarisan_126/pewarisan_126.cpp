34b0227894ddfaa487d62aa9be2f569ca395a4d4
mutiara126 <salsamutiara1812@gmail.com>
2023-05-22 12:36:25 +0800

    untuk membuat class pelajar

--- pewarisan_126/pewarisan_126.cpp
+++ pewarisan_126/pewarisan_126.cpp
@@ -14,7 +14,24 @@ class orang {
         cout << "orang dihapus\n" << endl;
     }
 
+    int jumlah(int a, int b) {
+        return a + b;
+    }
 
+};
+
+class pelajar : public orang {
+public:
+    string sekolah;
+
+    pelajar(string pNama, string pSekolah) :
+        orang(pNama),
+        sekolah(pSekolah) {
+        cout << "pelajar dibuat\n" << endl;
+    }
+    ~pelajar() {
+        cout << "pelajar dihapus\n" << endl;
+    }
 };
 
 
