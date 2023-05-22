1f83da06c21853a18e6a61b387865c15663b69d6
mutiara126 <salsamutiara1812@gmail.com>
2023-05-22 20:19:03 +0800

    membuat deklarasi ibu dan anak

--- Asosiasi/Asosiasi.cpp
+++ Asosiasi/Asosiasi.cpp
@@ -17,3 +17,40 @@ public:
 	void cetakDokter();
 };
 
+class dokter {
+public:
+	string nama;
+	vector<pasien*> daftar_pasien;
+
+	dokter(string pNama) :nama(pNama) {
+		cout << "Dokter \" " << nama << " \" ada\n";
+	}
+	~dokter() {
+		cout << "Dokter \" " << nama << "\" tidak ada\n";
+	}
+
+	void tambahPasien(pasien*);
+	void cetakPasien();
+};
+
+void pasien ::tambahDokter(dokter* pDokter) {
+	daftar_dokter.push_back(pDokter);
+}
+void pasien::cetakDokter()  {
+	cout << "Daftar Dokter yang menangani pasien \"" << this->nama << "\":\n";
+	for (auto& a : daftar_dokter) {
+		cout << a->nama << "\n";
+    }
+     cout << endl;
+} 
+void dokter::tambahPasien(pasien* pPasien) {
+	daftar_pasien.push_back(pPasien);
+	pPasien->tambahDokter(this);
+}
+void dokter::cetakPasien() {
+	cout << "daftar pasien dari dokter \"" << this->nama << "\":\n";
+	for (auto& a : daftar_pasien) {
+		cout << a->nama << "\n";
+	}
+	cout << endl;
+}
